2e7f90d71bf1d24991dc37f77761385173da7f23
Lucas de Paiva Ribeiro <proftilucas@outlook.com>
2024-10-30 21:18:12 -0300

    Ao atualizar a página, o histórico não some

--- conversor-de-moedas/src/app/servicos/conversao.service.ts
+++ conversor-de-moedas/src/app/servicos/conversao.service.ts
@@ -8,7 +8,7 @@ import axios from 'axios';
 export class ConversaoService {
   private apiUrl = 'https://v6.exchangerate-api.com/v6/11e6bb24c3f757dfdb38f2a8';
   
-  private historicoSubject = new BehaviorSubject<any[]>([]);
+  private historicoSubject = new BehaviorSubject<any[]>(this.carregarHistorico());
   public historico$ = this.historicoSubject.asObservable();
 
   private moedasSubject = new BehaviorSubject<{ simbolo: string; nome: string }[]>([]);
@@ -41,18 +41,29 @@ export class ConversaoService {
     }
   }
 
-  getHistoricoAtual() {
-    return this.historicoSubject.value;
+  private adicionarAoHistorico(conversao: any) {
+    const historicoAtual = this.historicoSubject.value;
+    const novoHistorico = [...historicoAtual, conversao];
+    this.atualizarHistorico(novoHistorico);
   }
 
   atualizarHistorico(novoHistorico: any[]) {
     this.historicoSubject.next(novoHistorico);
+    if (this.estaNoNavegador()) {
+      localStorage.setItem('historicoConversoes', JSON.stringify(novoHistorico));
+    }
   }
 
-  private adicionarAoHistorico(conversao: any) {
-    const historicoAtual = this.historicoSubject.value;
-    const novoHistorico = [...historicoAtual, conversao];
-    this.historicoSubject.next(novoHistorico);
+  private carregarHistorico() {
+    if (this.estaNoNavegador()) {
+      const historicoSalvo = localStorage.getItem('historicoConversoes');
+      return historicoSalvo ? JSON.parse(historicoSalvo) : [];
+    }
+    return [];
+  }
+
+  getHistoricoAtual() {
+    return this.historicoSubject.value;
   }
 
   private async carregarMoedas() {
@@ -66,5 +77,9 @@ export class ConversaoService {
       console.error('Erro ao obter moedas:', error);
       throw error;
     }
+  }
+
+  private estaNoNavegador(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
   }
 }
